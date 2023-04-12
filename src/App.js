0603d64ae7a613c4e2d8f864bd530cf4dcd7a70c
chyerin802 <chyerin802@gmail.com>
2023-04-13 01:53:26 +0900

    feat: #1 Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import TodoPage from './pages/TodoPage';
+import MainPage from './pages/MainPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/signin' element={<SignInPage />}></Route>
+      <Route path='/signup' element={<SignUpPage />}></Route>
+      <Route path='/todo' element={<TodoPage />}></Route>
+      <Route path='/*' element={<MainPage />}></Route>
+    </Routes>
   );
 }
 
