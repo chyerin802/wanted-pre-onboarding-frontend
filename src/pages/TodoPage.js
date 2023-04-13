c9f2a5d6baae6e91ef46c6485ff4b50bd8f361d7
chyerin802 <chyerin802@gmail.com>
2023-04-13 20:52:07 +0900

    fix: 간단한 오류 수정

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const TodoPage = () => {
-    return (
-        <div>
-            TodoPage
-        </div>
-    );
+  return <div>TodoPage</div>;
 };
 
 export default TodoPage;
