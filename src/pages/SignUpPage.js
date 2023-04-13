69952276e1cb77d5b3d7ec430cc923aecbfa7991
chyerin802 <chyerin802@gmail.com>
2023-04-14 04:26:35 +0900

    #13 feat: 요구사항에 따른 리다이렉트 기능 구현

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,9 @@
 import SignUpForm from 'components/SignUpForm';
+import { Navigate } from 'react-router-dom';
+import isLogin from 'utils/isLogin';
 
 const SignUpPage = () => {
+  if (isLogin()) return <Navigate to="/todo" />;
   return (
     <div>
       <div>SignUp</div>
