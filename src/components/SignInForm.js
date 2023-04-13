9c2374f288aa0f9579a60e15af1f79542a42f729
chyerin802 <chyerin802@gmail.com>
2023-04-13 20:47:26 +0900

    #6 feat: useForm 이용하여 로그인 폼 완성

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import useForm from '../hooks/useForm';
+import authValidate from '../utils/authValidate';
 
 const SignInForm = () => {
-    return (
-        <form>
-            <label htmlFor='email'>Email</label>
-            <input type='email' name='email' id="email" data-testid="email-input"></input>
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' id="password" data-testid="password-input"></input>
-            <button data-testid="signin-button">submit</button>
-        </form>
-    );
+  const { values, errors, touched, handleChange, handleSubmit } = useForm({
+    initialValues: { email: '', password: '' },
+    validate: authValidate,
+    onSubmit: () => {
+      alert('submit!');
+    },
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        data-testid="email-input"
+        value={values.email}
+        onChange={handleChange}
+      ></input>
+      {touched.email && errors.email && <span>{errors.email}</span>}
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        data-testid="password-input"
+        value={values.password}
+        onChange={handleChange}
+      ></input>
+      {touched.password && errors.password && <span>{errors.password}</span>}
+      <button
+        data-testid="signin-button"
+        disabled={Object.keys(errors).length !== 0}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default SignInForm;
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,13 +1,12 @@
-import React from 'react';
 import SignInForm from '../components/SignInForm';
 
 const SignInPage = () => {
-    return (
-        <div>
-            <div>SignIn</div>
-            <SignInForm></SignInForm>
-        </div>
-    );
+  return (
+    <div>
+      <div>SignIn</div>
+      <SignInForm></SignInForm>
+    </div>
+  );
 };
 
 export default SignInPage;
