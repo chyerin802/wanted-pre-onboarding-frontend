20b436a30335cbb50b49b4ec82f5f93db8969498
chyerin802 <chyerin802@gmail.com>
2023-04-15 02:30:45 +0900

    #18 feat: Todo 추가 시 목록에 반영되도록 구현

--- src/components/TodoCreateForm.js
+++ src/components/TodoCreateForm.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { createTodo } from 'api';
 
-const TodoCreateForm = () => {
+const TodoCreateForm = ({ todoList, setTodoList }) => {
   const [newTodo, setNewTodo] = useState('');
 
   const handleTodoChange = (e) => setNewTodo(e.target.value);
@@ -9,7 +10,7 @@ const TodoCreateForm = () => {
     e.preventDefault();
     try {
       const res = await createTodo(newTodo);
-      console.log(res);
+      setTodoList([...todoList, res]);
     } catch (err) {
       console.log(err);
     }
@@ -28,6 +29,11 @@ const TodoCreateForm = () => {
       </button>
     </form>
   );
+};
+
+TodoCreateForm.propTypes = {
+  todoList: PropTypes.array.isRequired,
+  setTodoList: PropTypes.func.isRequired,
 };
 
 export default TodoCreateForm;
