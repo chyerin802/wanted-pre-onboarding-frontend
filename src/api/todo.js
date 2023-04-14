58a41cdf3c1a9662c18eb381a0350916c223a704
chyerin802 <chyerin802@gmail.com>
2023-04-14 19:56:08 +0900

    #18 feat: createTodo, getTodos 비동기 함수 구현

---
+++ src/api/todo.js
@@ -0,0 +1,21 @@
+import { authInstance } from './instance';
+
+const API_PATH_TODO = '/todos';
+
+export const createTodo = async (body) => {
+  try {
+    await authInstance(API_PATH_TODO, body);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const getTodos = async () => {
+  try {
+    await authInstance(API_PATH_TODO);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
