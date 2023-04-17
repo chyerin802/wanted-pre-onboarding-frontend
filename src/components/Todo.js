5ae29a1c934940c4b320b630e240c15fab8939d3
chyerin802 <chyerin802@gmail.com>
2023-04-17 23:09:13 +0900

    #22 feat: todo 수정, 삭제 버튼

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -28,6 +28,8 @@ const Todo = ({ id, todo, isCompleted }) => {
         />
         <span>{todo}</span>
       </label>
+      <button data-testid="modify-button">수정</button>
+      <button data-testid="delete-button">삭제</button>
     </li>
   );
 };
