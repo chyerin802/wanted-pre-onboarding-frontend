e65d137306472e253420be83264c00436c6a6cc5
chyerin802 <chyerin802@gmail.com>
2023-04-19 15:22:20 +0900

    feat: 로그아웃 기능 구현

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,14 +1,27 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import TodoList from 'components/todo/TodoList';
 import TodoCreateForm from 'components/todo/TodoCreateForm';
 import Title from 'components/Title';
+import Button from 'components/Button';
 import { getTodos } from 'api';
 import isLogin from 'utils/isLogin';
+
+const LogOutBtnWrapper = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+`;
 
 const TodoPage = () => {
   const navigate = useNavigate();
   const [todoList, setTodoList] = useState([]);
+
+  const handleLogOutClick = () => {
+    localStorage.removeItem('access_token');
+    navigate('/signin');
+  };
 
   useEffect(() => {
     const asyncGetTodoList = async () => {
@@ -34,6 +47,11 @@ const TodoPage = () => {
       <Title>Todo</Title>
       <TodoCreateForm todoList={todoList} setTodoList={setTodoList} />
       <TodoList todoList={todoList} setTodoList={setTodoList} />
+      <LogOutBtnWrapper>
+        <Button onClick={handleLogOutClick} letter color="second">
+          로그아웃
+        </Button>
+      </LogOutBtnWrapper>
     </div>
   );
 };
